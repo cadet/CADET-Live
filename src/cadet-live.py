c0f903a21763eeacacc3c40a55a8c7f8038f4d36
Jannis Bergmann <j.be+github-public@posteo.de>
2025-12-08 10:15:26 +0100

    Move  all .py files to new src folder.

--- src/cadet-live.py
+++ src/cadet-live.py
@@ -13,7 +13,9 @@ config_file = config.get_config()
 
 configuration = config.config_to_source(config_file)
 
-sim_file = h5.load_h5_file("./cstr_one_inlet_one_mal(1).h5")
+h5_file_path = "./modelLibrary/cstr_one_inlet_one_mal.h5"
+
+sim_file = h5.load_h5_file(h5_file_path)
 
 client = mqtt.Client()
 
@@ -29,7 +31,7 @@ for i in range(0, 2):
     current_data = mqtt_client.user_data_get()
     data[0] = current_data["A"]
     print(sim_file["input"]["model"]["unit_001"]["INIT_C"][()])
-    h5.save_h5_file("./cstr_one_inlet_one_mal(1).h5", sim_file)
+    h5.save_h5_file(h5_file_path, sim_file)
 
      
     cadet = Cadet()
@@ -37,13 +39,13 @@ for i in range(0, 2):
 #    cadet.load_from_h5("./data.h5")
 #    cadet.run_simulation()
 #    print(vars(cadet))
-    result = cadet.run_h5("./cstr_one_inlet_one_mal(1).h5")
+    result = cadet.run_h5(h5_file_path)
     print(result.root.output.solution.unit_001.SOLUTION_BULK)
     time.sleep(10)
 
 
 
-h5.save_h5_file("./cstr_one_inlet_one_mal(1).h5", sim_file)
+h5.save_h5_file(h5_file_path, sim_file)
 
 mqtt.mqtt_stop(mqtt_client)
 
