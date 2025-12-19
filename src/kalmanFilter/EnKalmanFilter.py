b7fd32c8826e424a502e7102498a304c7457ef2e
AntoniaBerger <a.berger@fz-juelich.de>
2025-12-19 16:52:57 +0100

    Update KalmanFilter

--- src/kalmanFilter/EnKalmanFilter.py
+++ src/kalmanFilter/EnKalmanFilter.py
@@ -1,63 +1,33 @@
 import numpy as np
+import pandas as pd
+
 """
 ?Questions: 
 - Sollten die Messungen ahnung vom Model haben? | Ja
 ToDos:
-- [ ] Measurements soll warend eines Prozesses hinzugefuegzt werden koennen.
-- [ ] Messungen mussen erst transformiert werden um in den Zustandsraum zu passen. 
+- [ ] Implementiere eine stabile methode um den nächsten Zeitpunkt zu finden.
 """
 
+from Mesurement import DFProvider, MeasureProvider
 
-class Measurement:
-    
-    def __init__(self, 
-                 state_name: str,
-                 times: np.array,
-                 observed_states: np.array,
-                 noise: np.ndarray = np.array([[0.0]])):
-        
-        self.state_name = state_name
-        
-        if (len(times) != len(observed_states)):
-            raise ValueError("Times and observed states must have the same length.")
-        
-        self.time_stamp = times
-        self.observed_states = observed_states
-        self.noise = noise
+class StateEstimator:
+    pass
 
-    @property
-    def nMeasurements(self)-> int:
-        return len(self.observed_states)
-    
-    @property
-    def measurementTimes(self) -> np.array:
-        return self.time_stamp
-    
-    @property
-    def nObservedStates(self) -> int:
-        return self.observed_states.shape[1]
-    
-    def printMeasurements(self):
-        for t, obs in zip(self.time_stamp, self.observed_states):
-            print(f"Time: {t}, Observed State: {obs}")
+class EnKalmanFilter(StateEstimator):
 
-class EnKalmanFilter:
-
-    def __init__(self, 
+    def __init__(self,
                   integrator: object,
-                  measuremnts: Measurement,
                   ensemble_size: int,
                   process_noise: np.ndarray,
                   initial_state: np.ndarray,
                   initial_covariance: np.ndarray):
         
         self.integrator = integrator
-        self.measurements = measuremnts
         self.N_ens = ensemble_size
         self.noise = process_noise
         self.state = initial_state
         self.cov = initial_covariance
-        self.meas_index = 0
+        self.time_point = 0
 
         self.X_ens = np.random.multivariate_normal(self.state, self.cov, self.N_ens)
 
@@ -72,24 +42,30 @@ class EnKalmanFilter:
 
             self.X_ens[i] = sol + w
     
-    def update(self):
-        """Update the ensemble based on the measurement."""
+    def update(self, 
+               timepoint: float, 
+               measure: MeasureProvider
+               ):
         
-        y_meas = self.measurements.observed_states[self.meas_index]
-        Y_ens = np.array([y_meas + np.random.normal(0,self.measurements.noise)[0] for _ in range(self.N_ens)])
-
+        """Update the ensemble based on the measurement."""
+        if measure.noise == np.array([[0.0]]):
+            raise ValueError("Measurement noise must be defined for update step.")
+        
+        # Get measurement for the current timepoint
+        y_meas = measure.getRawMeasurement(timepoint)
+        Y_ens = np.array([y_meas + np.random.normal(0,measure.noise)[0] for _ in range(self.N_ens)])
 
         # Calculate mean
         x_mean = np.mean(self.X_ens, axis=0, keepdims=True)
         y_mean = np.mean(Y_ens, axis=0, keepdims=True)
 
         # Calculate deviations
-        X_dev = self.X_ens - x_mean
+        X_dev = self.X_ens - x_mean 
         Y_dev = Y_ens - y_mean
 
         # Calculate Kalman gain
         P_xy = (X_dev.T @ Y_dev) / (self.N_ens - 1)
-        P_yy = (Y_dev.T @ Y_dev) / (self.N_ens - 1) + self.measurements.noise
+        P_yy = (Y_dev.T @ Y_dev) / (self.N_ens - 1) + measure.noise
 
         K = P_xy @ np.linalg.inv(P_yy)
 
@@ -99,9 +75,18 @@ class EnKalmanFilter:
         
         # Update state and covariance
         self.state = np.mean(self.X_ens, axis=0)
-        self.cov = np.cov(self.X_ens.T)
+        self.cov = np.cov(self.X_ens.T) + 1e-6 * np.eye(self.nStates) # to ensure numerical stability
+    
+    @property
+    def nStates(self) -> int:
+        return self.state.shape[0]
 
-        self.meas_index += 1
+    @property
+    def nEnsembles(self) -> int:
+        return self.N_ens
+
+    def getStateValues(self,idxState) -> np.ndarray:
+        return self.state[idxState]
 
 
 if __name__ == "__main__":
@@ -153,18 +138,42 @@ if __name__ == "__main__":
 
         measurements.append(res['xf'][0].full().flatten()[0] + np.random.normal(0, 0.1))
             
-    meas = Measurement("X", np.array(time_points), np.array(measurements), np.array([[0.1]]))
 
     enkf = EnKalmanFilter(
         integrator=lambda x0: integrato(x0=x0)['xf'].full().flatten(),
-        measuremnts=meas,
         ensemble_size=50,
         process_noise=np.diag([0.01, 0.01, 0.01]),
         initial_state=np.array([1.0, 0.5, 2.0]),
         initial_covariance=np.diag([0.1, 0.1, 0.1])
     )
 
+    df = pd.DataFrame({
+        "t": time_points,
+        "X": measurements
+    })
+
+    meas = DFProvider(
+        df,
+        x_column="t",
+        y_columns=["X"],
+        noise=np.array([[0.01]])
+    )
+    
+    states = []
     for t in range(len(time_points)):
         enkf.propagate()
-        enkf.update()
+        enkf.update(t*dt, meas)
         print(f"Time: {time_points[t]}, Estimated State: {enkf.state}")
+        states.append(enkf.state.copy()[0])
+
+    #plot results
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(10, 6))
+    plt.plot(time_points, X_sim[1:], label='True State X', color='blue')
+    plt.scatter(time_points, measurements, label='Measurements', color='red', s=10)
+    plt.plot(time_points, states, label='Estimated State X', color='green')
+    plt.xlabel('Time')
+    plt.ylabel('State X')
+    plt.legend()
+    plt.title('Ensemble Kalman Filter State Estimation')
+    plt.show()
